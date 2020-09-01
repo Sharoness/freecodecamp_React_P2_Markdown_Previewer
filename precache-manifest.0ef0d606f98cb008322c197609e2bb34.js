6d45e64a23e9a0305933ca4b401a32f2b0faae10
Sharoness <sharong1234@hotmail.com>
2020-09-01 13:41:44 +0100

    Updates

---
+++ precache-manifest.0ef0d606f98cb008322c197609e2bb34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5496245e9225ff3efd15e5c7754e4e",
+    "url": "/freecodecamp_React_P2_Markdown_Previewer/index.html"
+  },
+  {
+    "revision": "8530ca1c5f57923245cb",
+    "url": "/freecodecamp_React_P2_Markdown_Previewer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "635194ab000c8fdef36f",
+    "url": "/freecodecamp_React_P2_Markdown_Previewer/static/css/main.4468823c.chunk.css"
+  },
+  {
+    "revision": "8530ca1c5f57923245cb",
+    "url": "/freecodecamp_React_P2_Markdown_Previewer/static/js/2.541a0a00.chunk.js"
+  },
+  {
+    "revision": "a5658da467a5e4ae636a09bf722c41d5",
+    "url": "/freecodecamp_React_P2_Markdown_Previewer/static/js/2.541a0a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635194ab000c8fdef36f",
+    "url": "/freecodecamp_React_P2_Markdown_Previewer/static/js/main.391828aa.chunk.js"
+  },
+  {
+    "revision": "cd5d70bd56abfe5b1c13",
+    "url": "/freecodecamp_React_P2_Markdown_Previewer/static/js/runtime-main.15a937af.js"
+  }
+]);
